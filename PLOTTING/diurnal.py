552ab30e00f1906cbc53faa982b83d541ed8c069
Adrian Mark Tompkins <tompkins@ictp.it>
2020-12-09 09:54:18 +0100

    interface

---
+++ PLOTTING/diurnal.py
@@ -0,0 +1,15 @@
+import matplotlib.pyplot as plt
+from netCDF4 import Dataset
+import getargs
+import toy_diffusion_2d
+
+dir="/scratch/tompkins/diff/"
+
+# get the defaults
+pars=toy_diffusion_2d.defaults()
+
+# overwrite with the following 
+diff_K=8000.
+
+
+td_stats_diffK8000.0_tausub10.0_crhad14.72_cin_radius10.0_diurn0.nc
