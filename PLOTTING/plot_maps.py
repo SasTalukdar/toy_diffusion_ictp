5662cb4b5923fe1cc5393192d6e2723504c724d1
Adrian Mark Tompkins <tompkins@ictp.it>
2022-09-08 13:34:29 +0200

    plotting routine

--- PLOTTING/plot_maps.py
+++ PLOTTING/plot_maps.py
@@ -1,92 +1,88 @@
-def map_plot(x,y,fld,title,day,ifig,exp,vmin,vmax,loc):
-    fig,ax=plt.subplots()
-    print (vmin,vmax)
-    img=ax.pcolormesh(x,y,fld,cmap='Spectral',vmin=vmin,vmax=vmax)
-    ax.set_title(title+" : day "+day)
-    #ax.axis([x.min(), x.max(), y.min(), y.max()])
-    ax.set_xlabel('km', fontsize=15)
-    ax.set_ylabel('km', fontsize=15)
-    ax.set_xlim(0,int(domain_x/1000.))
-    ax.set_ylim(0,int(domain_y/1000.))
-    # fudge on resolution
-    ax.scatter(dx/1000*loc[:,1],dy/1000*loc[:,0],s=10,marker="s",zorder=1,color="black",edgecolors="white")
-    plt.colorbar(img,ax=ax)
-    plt.savefig(odir+"map_"+title+"_"+exp+"_"+str(ifig).zfill(3)+".png")
-    plt.close(fig)
+#
+# plot code janked from main code
+#
+import numpy as np 
+import matplotlib.pyplot as plt
+from netCDF4 import Dataset
 
+odir="./"
+csfont = {'fontname':'Helvetica'}
+
+def map_plot(ax,fld,title,day,exp,vmin,vmax,loc,d2c1):
+    print (title)
+    img=ax.pcolormesh(x,y,fld,cmap='Spectral',vmin=vmin,vmax=vmax)
+    ax.set_title("day "+str(day),**csfont)
+    #ax.axis([x.min(), x.max(), y.min(), y.max()])
+    if axidx[0]==1:
+        ax.set_xlabel('km', fontsize=15)
+    if axidx[1]==0:
+        ax.set_ylabel('km', fontsize=15)
+    #ax.set_xlim(0,int(domain_x/1000.))
+    #ax.set_ylim(0,int(domain_y/1000.))
+    print(d2c1)
+    cs=ax.contour(x,y,d2c1,[2],colors='white')
+    ax.set_xticks(np.arange(0,600,100))
+    ax.set_yticks(np.arange(0,600,100))
+    if False:
+        ax.scatter([y*2 for x,y in convidx],[x*2 for x,y in convidx],s=5,marker="s",zorder=1,color="black",edgecolors="white")
+    plt.colorbar(img,ax=ax)
+    plt.subplots_adjust(left=0.12,right=0.82, bottom=0.1,top=0.9,wspace=0.25,hspace=0.4)
 
 
     # other plot stuff
 
-    
-    if lplot:
-        fig1,ax1=plt.subplots()
-        fig_ts,ax_ts=plt.subplots(nrows=3,ncols=2,sharex=True)
+slices=[i*8 for i in [5,20,30,100]]
 
-    axc=np.unravel_index(range(6),(2,3))
+dir="/home/netapp-clima/users/tompkins/diffusion/"
+file="td_maps_diffK7000_tausub15_crhad16.12_cin_radius6_diurn0.nc"
 
+ds=Dataset(dir+file)
 
-            if lplot:
-            ax_ts[0,0].plot(days,ncnv,label=sdiurn)
+crh=ds.variables["CRH"]
+d2c=ds.variables["D2C"]
+# division needed due to bug 
+x=np.array(ds.variables["X"])/2000
+y=np.array(ds.variables["Y"])/2000 
+time=np.array(ds.variables["time"])
 
+cnv_loc=0
 
-
-                if lplot and (it*dt)%(nfig_hr*3600)==0 and day>sfig_day:
-
-                    print("PLOT: day ",sday)        
-                    map_plot(x,y,cin[1,:,:],"CIN",sday,ifig,sdiurn,0,1,cnv_loc)
-                    map_plot(x,y,crh[1,:,:],"CRH",sday,ifig,sdiurn,0,1,cnv_loc)
-                    map_plot(x,y,prob,"P",sday,ifig,sdiurn,0,np.max(prob),cnv_loc)
-                    map_plot(x,y,prob_cin,"P-CIN",sday,ifig,sdiurn,0,np.max(prob_cin),cnv_loc)
-                    map_plot(x,y,prob_crh,"P-CRH",sday,ifig,sdiurn,0,np.max(prob_crh),cnv_loc)
-
-                    # correct these surface plots
-                    if False:
-                        fig=plt.figure()
-                        ax=plt.axes(projection='3d')
-                        ax.plot_surface(x,y,crh[1,:,:],cmap='viridis', edgecolor='none')
-                        ax.set_title('CRH: day '+str(ifig*nfig_hr/24))
-                        ax.set_zlim([0.4,1])
-                        fig.savefig("crh_3d_"+str(ifig).zfill(3)+".png")
-                        plt.close(fig)
-
-                        fig=plt.figure()
-                        ax=plt.axes(projection='3d')
-                        ax.plot_surface(x,y,cin[1,:,:],cmap='viridis', edgecolor='none')
-                        ax.set_title('CIN p: day '+str(ifig*nfig_hr/24))
-                        ax.set_zlim([0,1])
-                        fig.savefig("cin_3d_"+str(ifig).zfill(3)+".png")
-                        plt.close(fig)
-
-                    # line plot
-                    ax1.plot(x[mp,mp-40:mp+40],crh[1,mp,mp-40:mp+40])
-                    ax1.plot(x[mp,mp-40:mp+40],cin[1,mp,mp-40:mp+40])
-
-                    ifig+=1
+sday=10
+sdiurn=2
+ncols=nrows=2
+fig,ax=plt.subplots(nrows=nrows,ncols=ncols)
+print (slices)
+print (time.shape)
 
 
-        if lplot:
-            ax_ts[0,1].plot(days,crh_mean,label=sdiurn)
-            ax_ts[0,1].set_xlabel("Time(days)")
-            ax_ts[0,1].set_ylabel("CRH mean ")
+for idx,sday in enumerate(slices):
+    axidx=np.unravel_index(idx,(nrows,ncols))
+    print (axidx[0])
+    map_plot(ax[axidx],crh[sday,:,:],"CRH",time[sday]/86400.,sdiurn,0.3,1.05,cnv_loc,d2c[sday,:,:])
+plt.savefig(odir+file[:-3]+".png")
+plt.close(fig)
 
-            ax_ts[1,0].plot(days,crh_std,label=sdiurn)
-            ax_ts[1,0].set_xlabel("Time(days)")
-            ax_ts[1,0].set_ylabel("CRH standard dev ")
 
-            ax_ts[1,1].plot(days,crh_in_new,label=sdiurn)
-            ax_ts[1,1].set_xlabel("Time(days)")
-            ax_ts[1,1].set_ylabel("CRH conv new ")
+#map_plot(x,y,cin[1,:,:],"CIN",sday,ifig,sdiurn,0,1,cnv_loc)
+#map_plot(x,y,prob,"P",sday,ifig,sdiurn,0,np.max(prob),cnv_loc)
+#map_plot(x,y,prob_cin,"P-CIN",sday,ifig,sdiurn,0,np.max(prob_cin),cnv_loc)
+#map_plot(x,y,prob_crh,"P-CRH",sday,ifig,sdiurn,0,np.max(prob_crh),cnv_loc)
 
-            ax_ts[2,0].plot(days,crh_driest,label=sdiurn)
-            ax_ts[2,0].set_xlabel("Time(days)")
-            ax_ts[2,0].set_ylabel("CRH min ")
-            ax_ts[0,1].legend()
+# correct these surface plots
+if False:
+    fig=plt.figure()
+    ax=plt.axes(projection='3d')
+    ax.plot_surface(x,y,crh[1,:,:],cmap='viridis', edgecolor='none')
+    ax.set_title('CRH: day '+str(ifig*nfig_hr/24))
+    ax.set_zlim([0.4,1])
+    fig.savefig("crh_3d_"+str(ifig).zfill(3)+".png")
+    plt.close(fig)
 
-    # end of run
-    if lplot:
-        fig_ts.savefig(odir+"timeseries_"+sdiurn+".pdf")
-        fig1.savefig(odir+"lineplot.png")
-        plt.close(fig_ts)
-        plt.close(fig1)
-            
+    fig=plt.figure()
+    ax=plt.axes(projection='3d')
+    ax.plot_surface(x,y,cin[1,:,:],cmap='viridis', edgecolor='none')
+    ax.set_title('CIN p: day '+str(ifig*nfig_hr/24))
+    ax.set_zlim([0,1])
+    fig.savefig("cin_3d_"+str(ifig).zfill(3)+".png")
+    plt.close(fig)
+
